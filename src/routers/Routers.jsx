6f3c42f9a637b0628a9d410a5568df56ee44c8bc
Mohammed Amin Mosttefaoui <mohammed.amin.mostefaoui@gmail.com>
2024-06-13 19:57:16 +0100

    removing log

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -4,18 +4,9 @@ import Home from "../pages/Home"
 import ProjectDetails from "../pages/ProjectDetails"
 import OtherProjectDetails from "../pages/OtherProjectDetails"
 import NotFound from "../pages/NotFound"
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+
 function Routers() {
-    const location = useLocation();
 
-    useEffect(() => {
-      console.log('Location changed');
-      console.log('hash', location.hash); // #myHash
-console.log('pathname', location.pathname); // /myuser
-console.log('search', location.search); // ?page=10
-
-    }, [location]);
     return (
         <>
             <Routes>
