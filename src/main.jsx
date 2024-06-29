5a87469478a7a9647930804e393a7b36a741dcc0
Mohammed Amin Mosttefaoui <mohammed.amin.mostefaoui@gmail.com>
2024-06-29 23:10:24 +0100

    revert fix router

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from "react-redux";
 import store from "../src/app/store.js";
 import { ToastContainer } from "react-toastify";
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <ToastContainer
           position="bottom-right"
           autoClose={5000}
@@ -24,7 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           theme="light"
         />
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
